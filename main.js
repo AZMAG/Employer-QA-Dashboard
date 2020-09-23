7021589f6a4f63bd8771268871e24181aa8085f7
Jack Fairfield <jfairfield@azmag.gov>
2020-09-22 17:04:24 -0700

    add line chart

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ async function startUp() {
 }
 
 async function updateKendoGrid(field) {
-  window.kendo.ui.progress($('#grid'), true);
+  resetChart();
+  kendo.ui.progress($('#grid'), true);
   const data = await getDataByGroupField(field);
-  console.log(data);
   const dataSource = new kendo.data.DataSource({
     data,
     sort: {
@@ -19,10 +19,20 @@ async function updateKendoGrid(field) {
   });
   const grid = $('#grid').data('kendoGrid');
   grid.setDataSource(dataSource);
-  window.kendo.ui.progress($('#grid'), false);
+  kendo.ui.progress($('#grid'), false);
+}
+function resetChart() {
+  try {
+    $('#chart').data('kendoChart').destroy();
+    $('#chart').empty();
+    $('#chart').removeClass('k-chart');
+    $('#btnRemoveChart').hide();
+  } catch (error) {}
 }
 
-function getYearSumFields() {
+$('#btnRemoveChart').click(resetChart);
+
+function getYearSumFields(type) {
   let fields = [];
   years.forEach((year) => {
     fields.push({
@@ -38,19 +48,30 @@ function getYearSumFields() {
       width: 150,
     });
   });
+  if (type) {
+    return fields.filter((row) => row.field.includes(type));
+  }
   return fields;
 }
 
 function setupKendoGrid() {
   window.kendo.ui.progress($('#grid'), true);
   let yearFields = getYearSumFields();
-
   $('#grid').kendoGrid({
     height: 550,
     width: 1200,
     scrollable: {
       virtual: true,
     },
+    change: function (e) {
+      const [row] = this.select();
+      const data = this.dataItem(row);
+      const title = `${$('#field').data('kendoDropDownList').value()} - ${
+        data.groupValue
+      }`;
+      setupKendoChart(data, title);
+    },
+    selectable: true,
     sortable: true,
     columns: [
       {
@@ -62,6 +83,81 @@ function setupKendoGrid() {
       },
       ...getYearSumFields(),
     ],
+  });
+}
+
+function setupKendoChart(data, title) {
+  $('#btnRemoveChart').show();
+  let busData = years.map((year) => data['BusSum' + year]);
+  let empData = years.map((year) => data['EmpSum' + year]);
+  let bufferAxis = 0.1;
+  let busMax = Math.max(...busData.filter((num) => num));
+  let empMax = Math.max(...empData.filter((num) => num));
+  console.log(busMax, empMax);
+
+  let labels = $('#chart').kendoChart({
+    title: {
+      text: title,
+    },
+    legend: {
+      position: 'bottom',
+    },
+    chartArea: {
+      background: '',
+    },
+    seriesDefaults: {
+      type: 'line',
+      style: 'smooth',
+      missingValues: 'gap',
+    },
+    series: [
+      {
+        name: 'Employees',
+        data: empData,
+        axis: 'emp',
+        color: 'blue',
+      },
+      {
+        name: 'Businesses',
+        data: busData,
+        axis: 'bus',
+        color: 'red',
+      },
+    ],
+    valueAxes: [
+      {
+        name: 'bus',
+        color: 'blue',
+        min: 0,
+        max: busMax + busMax * bufferAxis,
+        labels: {
+          format: '{0:n0}',
+        },
+      },
+      {
+        name: 'emp',
+        color: 'red',
+        min: 0,
+        max: empMax + empMax * bufferAxis,
+        labels: {
+          format: '{0:n0}',
+        },
+      },
+    ],
+    tooltip: {
+      visible: true,
+      shared: true,
+      format: 'N0',
+    },
+    categoryAxis: {
+      categories: years,
+      majorGridLines: {
+        visible: false,
+      },
+      labels: {
+        rotation: 'auto',
+      },
+    },
   });
 }
 
